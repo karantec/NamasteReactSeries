6597465e13c40ea541f00713d5cbeca663cf1d0c
karanrana <karanrana3095@gmail.com>
2024-02-03 05:40:23 +0530

    elementwhyrequiredjsx

---
+++ App.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * <div id="parent">
+ * <div id="child">
+ * <h1 Ima h1 tag</h1>
+ * <h2> Ima h2 tag</h2>
+ * </div> 
+ * <div id="child2">
+ * <h1 Ima h1 tag</h1>
+ * <h2> Ima h2 tag</h2>
+ * </div> 
+ *
+ *  ReactElement(object)=>HTML(Browser Understandable)
+ */
+const parent = React.createElement(
+    'div',
+    { id: "parent" },
+   [ React.createElement(
+      'div',
+      { id: "child" },[
+      React.createElement(
+        'h1',
+        {},
+        "Ima h1 tag"
+      ),
+      React.createElement("h2",{},"I am an h2 tag"),
+      ])],
+        [ React.createElement(
+            'div',
+            { id: "child2" },[
+            React.createElement(
+            'h1',
+            {},
+            "Ima h1 tag"
+            ),
+            React.createElement("h2",{},"I am an h2 tag"),
+            ])]
+  );
+  //JSX =>because it exists in the React package
+  console.log(parent);
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(parent);
+  
+// const heading=React.createElement('h1',{id:"heading", xyz:"abc"},
+// "Hello World from React");
+// console.log(heading);
+
