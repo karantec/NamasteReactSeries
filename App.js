a219088fef867376d0a1d9f8f2e91d9d8435acf1
karanrana <karanrana3095@gmail.com>
2024-02-11 03:37:52 +0530

    firstpart

--- App.js
+++ App.js
@@ -13,26 +13,14 @@
  *  ReactElement(object)=>HTML(Browser Understandable)
  */
 const parent = React.createElement(
-    'div',
-    { id: "parent" },
-   [ React.createElement(
-      'div',
-      { id: "child" },[
-      React.createElement(
-        'h1',
-        {},
-        "Ima h1 tag"
-      ),
+    'div',{ id: "parent" },
+   [
+     React.createElement( 'div', { id: "child" },[
+      React.createElement('h1',{},"Ima h1 tag"),
       React.createElement("h2",{},"I am an h2 tag"),
       ])],
-        [ React.createElement(
-            'div',
-            { id: "child2" },[
-            React.createElement(
-            'h1',
-            {},
-            "Ima h1 tag"
-            ),
+        [ React.createElement('div', { id: "child2" },[
+            React.createElement('h1', {}, "Ima h1 tag"),
             React.createElement("h2",{},"I am an h2 tag"),
             ])]
   );
